bb018a48ff122b34e732e9cef4ed74ba725231a5
guocg <guochenguang@163.com>
2019-11-04 21:23:15 +0800

    商品管理模块

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpSession;
  */
 @Controller
 @RequestMapping("/manage/category")
-public class categoryManageController {
+public class CategoryManageController {
 
     @Autowired
     private IUserService iUserService;
